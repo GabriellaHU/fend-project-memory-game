74b43d2e739b474635fd2db40dbf51a568fe6ad4
Gabriella-Karacsonyi <gabi.karacsonyi@gmail.com>
2018-05-13 19:26:28 +0200

    add icons - icon classes - to the individual cards
    - create an Array with the classes
    - add the classes with the for loop to the createDeck() function

--- js/app.js
+++ js/app.js
@@ -1,47 +1,63 @@
-/*
- * Create a list that holds all of your cards
- */
+// new array to hold all card icon classes
+// TODO get rid off duplications
+const cardIconClasses = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-bomb', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-bomb', 'fa-leaf', 'fa-bicycle']
+
+
+
 function createDeck() {
 
+
+   // * Create a list that holds all of my cards
   const deckList = document.createElement('ul');
   deckList.classList.add('deck');
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+ // shuffle the list of cards using the provided "shuffle" method below
 
+  // loop through each card and create its HTML
   for (let cardNum = 0; cardNum < 16; cardNum++) {
      const newCard = document.createElement('li');
-     newCard.classList.add('card');
+     newCard.classList.add('card', 'match');
 
+     // newCard.textContent = 'number' + (cardNum+1);
+     const newIcon = document.createElement('i');
+     // newIcon.insertAdjacentText('afterbegin', (cardNum+1));
+
+     let iconNum = cardIconClasses[cardNum];
+     console.log(iconNum);
+     newIcon.classList.add('fa', iconNum);
+
+     newCard.appendChild(newIcon);
      deckList.appendChild(newCard);
+
    }
 
-   document.body.querySelector('.container').appendChild(deckList);
+  // add each card's HTML to the page
+  document.body.querySelector('.container').appendChild(deckList);
+
+  // shuffle();
 
 }
 
 createDeck();
 
+
+
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle() {
+function shuffle(cardArray) {
 
   // access and store the children of the deckList in the DOM
-  const cardNodeList = document.querySelectorAll('.card');
+  let deckList = document.querySelector('.deck');
+  // console.log(deckList.children);
+  let cardNodeList = document.querySelectorAll('.card');
   // console.log(cardNodeList);
 
   // convert NodeList to Array
   let cardArray = [];
   for(let i = cardNodeList.length; i--; cardArray.unshift(cardNodeList[i]));
-  console.log(cardArray);
-  // console.log(cardArray.length);
 
-  var currentIndex = cardArray.length, temporaryValue, randomIndex;
+  let currentIndex = cardArray.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
+  while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = cardArray[currentIndex];
@@ -49,10 +65,18 @@ function shuffle() {
         cardArray[randomIndex] = temporaryValue;
     }
 
-    return cardArray;
+  return cardArray;
+
+  document.body.querySelector('.container').querySelector('.deck').removeChildren();
+  console.log('hurray');
+
+ //  for (let newCardNum = 0; newCardNum < 16; newCardNum++) {
+ //     deckList.appendChild(cardNodeList[newCardNum]);
+ // }
+
 }
 
-shuffle();
+
 
 
 /*
